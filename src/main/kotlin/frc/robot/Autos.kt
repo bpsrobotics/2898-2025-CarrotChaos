e837da85e66991e46524935e60c197a804cc415f
BubbleShade <ozypfl@gmail.com>
2025-09-16 19:53:05 -0700

    Drivetrain cleanup (Should be tested on practice robot)

--- src/main/kotlin/frc/robot/Autos.kt
+++ src/main/kotlin/frc/robot/Autos.kt
@@ -17,9 +17,9 @@ object Autos {
 
     init {
         AutoBuilder.configure(
-            Drivetrain::getPose,  // Robot pose supplier
+            { Drivetrain.pose },  // Robot pose supplier
             Drivetrain::resetOdometry,  // Method to reset odometry (will be called if your auto has a starting pose)
-            Drivetrain::getRobotVelocity,  // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+            { Drivetrain.robotVelocity },  // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
             driveConsumer,  // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
             PPHolonomicDriveController( // PPolonomicController is the built-in path following controller for holonomic drive trains
                 PIDConstants(TranslationP, TranslationI, TranslationD),  // Translation PID constants
@@ -30,6 +30,37 @@ object Autos {
             Drivetrain// Reference to this subsystem to set requirements
         )
     }
+    /**
+     * Gets a command that follows a path created in PathPlanner.
+     * @param pathName The path's file name.
+     * @param setOdomAtStart Whether to update the robot's odometry to the start pose of the path.
+     * @return A command that follows the path.
+     */
+//    fun getAutonomousCommand(
+//        autoName: String,
+//       setOdomAtStart: Boolean
+//    ): Command {
+//        var startPosition: Pose2d = Pose2d()
+//        if(PathPlannerAuto.getStaringPoseFromAutoFile(autoName) == null) {
+//            startPosition = PathPlannerAuto.getPathGroupFromAutoFile(autoName)[0].startingDifferentialPose
+//        } else {
+//            startPosition = PathPlannerAuto.getStaringPoseFromAutoFile(autoName)
+//        }
+//
+//        if(DriverStation.getAlliance() == Optional.of(Alliance.Red)){
+//            startPosition = GeometryUtil.flipFieldPose(startPosition)
+//        }
+//
+//        if (setOdomAtStart)
+//        {
+//            if (startPosition != null) {
+//                resetOdometry(startPosition)
+//            }
+//        }
+//
+//        // TODO: Configure path planner's AutoBuilder
+//        return PathPlannerAuto(autoName)
+//    }
 
 
 }
