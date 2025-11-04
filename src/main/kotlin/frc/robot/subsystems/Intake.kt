79ac600228537b8b13f6a8303971daf8199f1d74
antho <anthonyvirayav@gmail.com>
2025-11-03 18:18:13 -0800

    motor

--- src/main/kotlin/frc/robot/subsystems/Intake.kt
+++ src/main/kotlin/frc/robot/subsystems/Intake.kt
@@ -1,11 +1,27 @@
 package frc.robot.subsystems
 
+import com.revrobotics.spark.SparkBase
 import com.revrobotics.spark.SparkLowLevel
 import com.revrobotics.spark.SparkMax
+import com.revrobotics.spark.config.SparkBaseConfig
+import com.revrobotics.spark.config.SparkMaxConfig
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.RobotMap
 
 object Intake : SubsystemBase() {
     private val intakeMotor = SparkMax(RobotMap.IntakeId, SparkLowLevel.MotorType.kBrushless)
+    private val IntakeConfig: SparkMaxConfig = SparkMaxConfig()
 
+    init {
+        // Intake motor initialisation stuff
+        IntakeConfig
+            .idleMode(SparkBaseConfig.IdleMode.kBrake)
+            .smartCurrentLimit(20)
+
+        intakeMotor.configure(
+            IntakeConfig,
+            SparkBase.ResetMode.kResetSafeParameters,
+            SparkBase.PersistMode.kPersistParameters
+        )
+    }
 }
